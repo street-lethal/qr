5eaf23aca296bdb347e77347e07c508860ba975d
Street Lethal <street.lethal.57@gmail.com>
2019-04-27 16:57:26 +0900

    qr_maker

--- qr_maker.rb
+++ qr_maker.rb
@@ -8,13 +8,21 @@ class QrMaker
     @qr = RQRCode::QRCode.new(str)
   end
 
-  def exec
-    base64 = @qr.to_img.resize(200, 200).to_data_url
+  def print
     puts base64
   end
 
   def png
-    png = @qr.to_img
-    png.resize(200, 200).save('out.png')
+    img.save('out.png')
+  end
+
+  private
+
+  def img
+    @qr.to_img.resize(200, 200)
+  end
+
+  def base64
+    img.to_data_url
   end
 end
